6598545ea8ad4863db8f1f76a0e9e18ad3a72b2d
c0repwn3r <coredoescode@gmail.com>
2021-11-13 05:33:52 -0500

    Add project files

---
+++ cmake-build-debug/CMakeFiles/xkls.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/xkls.dir/main.cpp.o"
+  "xkls"
+  "xkls.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/xkls.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/xkls.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for xkls.
+# This may be replaced when dependencies are built.
